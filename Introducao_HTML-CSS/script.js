eeadde067f2c0a5221011f3a770c7d8ce74e839c
Yuri Motoshima <yurimotoshima@gmail.com>
2020-04-22 11:41:49 -0300

    update

--- Introducao_HTML-CSS/script.js
+++ Introducao_HTML-CSS/script.js
@@ -1,12 +1,14 @@
 const modalOverlay = document.querySelector('.modal-overlay');
 const cards = document.querySelectorAll('.card')
 
-for (let card of cards){
-    card.addEventListener("click",function(){
+for (let card of cards) {
+    card.addEventListener("click", function () {
         modalOverlay.classList.add('active')
+        modalOverlay.querySelector('iframe').scr =  `https://www.youtube.com/embed/xWKRXJ7b9Lw`
+
     })
 }
 
-document.querySelector('.close-modal').addEventListener("click", function(){
+document.querySelector('.close-modal').addEventListener("click", function () {
     modalOverlay.classList.remove('active')
 })
