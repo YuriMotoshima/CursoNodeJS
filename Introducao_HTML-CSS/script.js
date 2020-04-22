00d1cd66b092d20140c8d77f823490cf82ab65be
Yuri Motoshima <yurimotoshima@gmail.com>
2020-04-22 12:02:55 -0300

    update

--- Introducao_HTML-CSS/script.js
+++ Introducao_HTML-CSS/script.js
@@ -1,14 +1,26 @@
-const modalOverlay = document.querySelector('.modal-overlay');
+const modalOverlay = document.querySelector('.modal-overlay')
 const cards = document.querySelectorAll('.card')
 
 for (let card of cards) {
     card.addEventListener("click", function () {
+        const videoId = card.getAttribute("id");
         modalOverlay.classList.add('active')
-        modalOverlay.querySelector('iframe').scr =  `https://www.youtube.com/embed/xWKRXJ7b9Lw`
+        modalOverlay.querySelector('iframe').src =  `https://www.youtube.com/embed/${videoId}`
 
     })
 }
 
 document.querySelector('.close-modal').addEventListener("click", function () {
     modalOverlay.classList.remove('active')
+    modalOverlay.querySelector("iframe").src = ""
 })
+
+/* 
+
+- document. é qualquer TAG do HTML;
+- .querySelector('.modal-overlay') seleciona uma TAG especificada do HTML;
+- .addEventListener("click", function () {}) faz a leitura de algum evento no HTML e após a leitura executa uma função;
+- .classList.add() ou .classList.remove() esse atributo seleciona a classe de uma TAG e adiciona, remove nomes nesta classe entre outras coisas;
+- .getAttribute() seleciona qualquer atributo existente em uma TAG do HTML;
+
+*/
