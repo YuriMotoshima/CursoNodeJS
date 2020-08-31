85e940eadc7b4bc979e8fcf050cbfc7901338fe0
Yuri Motoshima <yurimotoshima@gmail.com>
2020-08-31 06:50:45 -0300

    Update

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -12,7 +12,7 @@ const server = express()
 
 server.use(express.static('public'))
 
-server.set("view engine", "html")
+server.set("view engine", "njk")
 
 nunjucks.configure("views", {
     express:server
