fcb64ea88386dc4ce119b7fe6ab5fd5630f0fd0c
Yuri Motoshima <yurimotoshima@gmail.com>
2020-08-31 06:58:37 -0300

    Update

---
+++ BackEnd/data.js
@@ -0,0 +1,38 @@
+module.exports = [
+    {
+        id : "xWKRXJ7b9Lw",
+        title : "Como se destacar na carreira? Dicas e técnicas | Podcast FalaDev #19<",
+        duration : "32 min",
+        price : "Free"
+    },
+    {
+        id : "vqrjFnq3-uo",
+        title : "Criando Player de Áudio com Javascript | Mayk Brito",
+        duration : "105 min",
+        price : "Free"
+    },
+    {
+        id : "2alg7MQ6_sI",
+        title : "Como usar Git e Github na prática: Guia para iniciantes | Mayk Brito",
+        duration : "34 min",
+        price : "Free"
+    },
+    {
+        id : "HN1UjzRSdBk",
+        title : "Desvendando o CSS Grid na prática | Mayk Brito",
+        duration : "32 min",
+        price : "Free"
+    },
+    {
+        id : "ghTrp1x_1As",
+        title : "O que é API? REST e RESTful? | Mayk Brito",
+        duration : "32 min",
+        price : "Free"
+    },
+    {
+        id : "Rt-xG_VzD6M",
+        title : "Criando uma CLI com NodeJS e Gluegun | Diego Fernandes",
+        duration : "32 min",
+        price : "Free"
+    }
+]
